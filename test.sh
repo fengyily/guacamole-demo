44ee442695ad57b7b0f1d432368227e2f0b8dac1
Fengyily <Fengyily@noreply.gitcode.com>
2025-11-14 22:52:52 +0800

    fix: test

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ mvn clean package -DskipTests
 
 mv ./guacamole/target/guacamole-auth-dynamic-core-1.0.0.jar ./extensions/
 
-docker compose down && docker compose up -d
+docker compose -f docker-compose-mac.yaml down && docker compose -f docker-compose-mac.yaml up -d
